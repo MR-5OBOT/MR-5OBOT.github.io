78f4475e0b4430f8276fc1f94aa57cc1e22f9422
MR-5OBOT <yassir7m7@gmail.com>
2026-07-04 13:34:38 +0100

    v2: remove embedded old-website (now its own branch: v3)

--- old-website/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  // For GitHub Pages later, set:
-  //   site: 'https://mr-5obot.github.io',
-  //   base: '/fsociety-website',
-});
